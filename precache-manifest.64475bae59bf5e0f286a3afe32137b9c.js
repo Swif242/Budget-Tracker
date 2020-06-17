b87a7227c6f80eba18ab95f5fad55c96306d6e15
Jeremy Nelson <swif242@hotmail.com>
2020-06-16 19:36:50 -0700

    Updates

---
+++ precache-manifest.64475bae59bf5e0f286a3afe32137b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3940443f78286e692fb25c885e5f6f99",
+    "url": "/Budget-Tracker/index.html"
+  },
+  {
+    "revision": "1ebbf9cdfe9fda04bc0d",
+    "url": "/Budget-Tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6dca16d798dc160f60a9",
+    "url": "/Budget-Tracker/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "1ebbf9cdfe9fda04bc0d",
+    "url": "/Budget-Tracker/static/js/2.64ba3ab2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Budget-Tracker/static/js/2.64ba3ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dca16d798dc160f60a9",
+    "url": "/Budget-Tracker/static/js/main.b589507b.chunk.js"
+  },
+  {
+    "revision": "f0c438f4f5e8c90d84c1",
+    "url": "/Budget-Tracker/static/js/runtime-main.abea1c1a.js"
+  }
+]);
