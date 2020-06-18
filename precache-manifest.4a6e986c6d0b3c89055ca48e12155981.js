5e8562500ae275daef2dde0291e50a94a91b26d1
Jeremy Nelson <swif242@hotmail.com>
2020-06-17 20:18:24 -0700

    Updates

--- precache-manifest.4a6e986c6d0b3c89055ca48e12155981.js
+++ precache-manifest.4a6e986c6d0b3c89055ca48e12155981.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "69e7d75441106af0433769bf0413d78c",
+    "revision": "2853bf4cfa80784b9feaf81c2ee5df61",
     "url": "/Budget-Tracker/index.html"
   },
   {
-    "revision": "e4144cfc296b3ac0a346",
+    "revision": "12a9ff362e101ac336bc",
     "url": "/Budget-Tracker/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "e621470cd2ead4ef9dfc",
+    "revision": "2e8f84c8ac7840e9870d",
     "url": "/Budget-Tracker/static/css/main.cb9e985a.chunk.css"
   },
   {
-    "revision": "e4144cfc296b3ac0a346",
-    "url": "/Budget-Tracker/static/js/2.163ac54a.chunk.js"
+    "revision": "12a9ff362e101ac336bc",
+    "url": "/Budget-Tracker/static/js/2.2c4371c8.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/Budget-Tracker/static/js/2.163ac54a.chunk.js.LICENSE.txt"
+    "url": "/Budget-Tracker/static/js/2.2c4371c8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e621470cd2ead4ef9dfc",
-    "url": "/Budget-Tracker/static/js/main.00e53951.chunk.js"
+    "revision": "2e8f84c8ac7840e9870d",
+    "url": "/Budget-Tracker/static/js/main.864e3b93.chunk.js"
   },
   {
     "revision": "f0c438f4f5e8c90d84c1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Budget-Tracker/precache-manifest.43ed7d09354073fc36f815b4d7054e47.js"
+  "/Budget-Tracker/precache-manifest.4a6e986c6d0b3c89055ca48e12155981.js"
 );
 
 self.addEventListener('message', (event) => {
